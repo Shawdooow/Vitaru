bc8bc814af8bb090d50180878a97a7827c01a084
Shawdooow <banhammer1915@gmail.com>
2020-03-26 20:45:02 -0400

    switch over from GL46 to DX12

--- Vitaru/Vitaru.cs
+++ Vitaru/Vitaru.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) 2018-2019 Shawn Bozek.
 // Licensed under EULA https://docs.google.com/document/d/1xPyZLRqjLYcKMxXLHLmA5TxHV-xww7mHYVUuWLt2q9g/edit?usp=sharing
 
-using Prion.Application;
+using Prion.Game;
 
 namespace Vitaru
 {
-    public class Vitaru : Application
+    public class Vitaru : Game
     {
         public static void Main(string[] args)
         {
