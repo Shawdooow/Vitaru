684ebec146c9a0d274f9da246edf85c8f76e5667
Shawdooow <banhammer1915@gmail.com>
2020-04-07 12:41:52 -0400

    load the medicine image and paint it

--- Vitaru/MainMenuRoot.cs
+++ Vitaru/MainMenuRoot.cs
@@ -1,5 +1,6 @@
 ﻿using System.Drawing;
 using System.Numerics;
+using Prion.Game;
 using Prion.Game.Graphics.Drawables;
 using Prion.Game.Graphics.Layers;
 using Prion.Game.Graphics.Roots;
@@ -17,7 +18,7 @@ namespace Vitaru
                 ShaderProgram = Sprite.SpriteProgram,
                 ParentScaling = Axes.Both,
 
-                Child = new Box
+                Child = new Sprite(Game.TextureStore.GetTexture("medicine.png"))
                 {
                     ParentScaling = Axes.Both,
                     Size = new Vector2(0.5f),
