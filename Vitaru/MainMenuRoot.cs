939795224f65ce37494cdf8359a39f14ba21a26b
Shawdooow <banhammer1915@gmail.com>
2020-04-13 16:31:30 -0400

    Update MainMenuRoot.cs

--- Vitaru/MainMenuRoot.cs
+++ Vitaru/MainMenuRoot.cs
@@ -1,12 +1,17 @@
 ﻿// Copyright (c) 2018-2020 Shawn Bozek.
 // Licensed under EULA https://docs.google.com/document/d/1xPyZLRqjLYcKMxXLHLmA5TxHV-xww7mHYVUuWLt2q9g/edit?usp=sharing
 
+using System.Collections.Generic;
 using System.Drawing;
+using System.Numerics;
+using OpenTK.Input;
 using Prion.Game;
 using Prion.Game.Graphics.Drawables;
 using Prion.Game.Graphics.Layers;
 using Prion.Game.Graphics.Roots;
 using Prion.Game.Graphics.Sprites;
+using Prion.Game.Input.Handlers;
+using Prion.Game.Input.Receivers;
 
 namespace Vitaru
 {
@@ -14,10 +19,12 @@ namespace Vitaru
     {
         public MainMenuRoot()
         {
-            Layers.ShaderProgram = Sprite.SpriteProgram;
-            Add(new Layer2D<Sprite>
+            Player player = new Player();
+
+            Add(player.InputHandler);
+
+            Add(new SpriteLayer
             {
-                ShaderProgram = Sprite.SpriteProgram,
                 ParentScaling = Axes.Both,
 
                 Children = new[]
@@ -31,9 +38,94 @@ namespace Vitaru
                         ParentScaling = Axes.Both,
                         Color = Color.Black,
                         Alpha = 0.5f,
-                    }
+                    },
+                    player,
                 },
             });
+        }
+
+        private class Player : Sprite, IHasInputKeys<VitaruAction>
+        {
+            public BindInputHandler<VitaruAction> InputHandler { get; set; }
+
+            public Player() : base(Game.TextureStore.GetTexture("Sakuya Izayoi.png"))
+            {
+                ParentOrigin = Mounts.Center;
+                InputHandler = new VitaruInputManager();
+                Size = new Vector2(100);
+                Color = Color.Blue;
+            }
+
+            public override void PreRender()
+            {
+                base.PreRender();
+                Position = GetNewPlayerPosition(0.0025f);
+            }
+
+            public bool Pressed(VitaruAction t)
+            {
+                return true;
+            }
+
+            public bool Released(VitaruAction t)
+            {
+                return true;
+            }
+
+            protected virtual Vector2 GetNewPlayerPosition(double playerSpeed)
+            {
+                Vector2 playerPosition = Position;
+
+                double yTranslationDistance = playerSpeed * Clock.ElapsedTime;
+                double xTranslationDistance = playerSpeed * Clock.ElapsedTime;
+
+                if (InputHandler.Actions[VitaruAction.Slow])
+                {
+                    xTranslationDistance /= 2d;
+                    yTranslationDistance /= 2d;
+                }
+
+                if (InputHandler.Actions[VitaruAction.Up])
+                    playerPosition.Y += (float)yTranslationDistance;
+                if (InputHandler.Actions[VitaruAction.Left])
+                    playerPosition.X -= (float)xTranslationDistance;
+                if (InputHandler.Actions[VitaruAction.Down])
+                    playerPosition.Y -= (float)yTranslationDistance;
+                if (InputHandler.Actions[VitaruAction.Right])
+                    playerPosition.X += (float)xTranslationDistance;
+
+                //if (!VitaruPlayfield.BOUNDLESS)
+                //{
+                //    playerPosition = Vector2.ComponentMin(playerPosition, ChapterSet.PlayfieldBounds.Zw);
+                //    playerPosition = Vector2.ComponentMax(playerPosition, ChapterSet.PlayfieldBounds.Xy);
+                //}
+
+                return playerPosition;
+            }
+        }
+
+        private class VitaruInputManager : ConfigedInputHandler<VitaruAction>
+        {
+            public override string Filename => "vitaruinput";
+
+            protected override void OnInitialiseDefaults()
+            {
+                InputSettings.SetValue(VitaruAction.Up, Key.W);
+                InputSettings.SetValue(VitaruAction.Down, Key.S);
+                InputSettings.SetValue(VitaruAction.Left, Key.A);
+                InputSettings.SetValue(VitaruAction.Right, Key.D);
+                InputSettings.SetValue(VitaruAction.Slow, Key.ShiftLeft);
+            }
+        }
+
+        private enum VitaruAction
+        {
+            Up,
+            Down,
+            Left,
+            Right,
+            Slow,
+            Shoot,
         }
     }
 }
