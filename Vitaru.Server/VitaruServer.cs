a4eeca3c690c1a4ed73ca1f02da5950c872cae4c
Shawdooow <banhammer1915@gmail.com>
2020-04-17 19:06:12 -0400

    ship server.exe with vitaru.exe

---
+++ Vitaru.Server/VitaruServer.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2018-2020 Shawn Bozek.
+// Licensed under EULA https://docs.google.com/document/d/1xPyZLRqjLYcKMxXLHLmA5TxHV-xww7mHYVUuWLt2q9g/edit?usp=sharing
+
+using Prion.Application;
+
+namespace Vitaru.Server
+{
+    public class VitaruServer : Application
+    {
+        public static void Main(string[] args)
+        {
+            using (VitaruServer server = new VitaruServer(args))
+                server.Start();
+        }
+
+        protected VitaruServer(string[] args) : base("vitaru", args)
+        {
+        }
+    }
+}
