95adf511b1b50e5f7cc63d0fcd545ceb8ab72b45
Shawdooow <banhammer1915@gmail.com>
2020-04-19 11:39:50 -0400

    Create LevelData.cs

---
+++ Vitaru/Editor/LevelData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vitaru.Characters.Enemies;
+using Vitaru.Server.Server;
+
+namespace Vitaru.Editor
+{
+    public class LevelData : Level
+    {
+        public List<Enemy> Enemies { get; set; }
+    }
+}
