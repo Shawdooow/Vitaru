7719e67c4db40b83b239395f3948608c1270c02a
Shawdooow <banhammer1915@gmail.com>
2020-04-19 11:39:39 -0400

    Create SeekableClock.cs

---
+++ Vitaru/Editor/SeekableClock.cs
@@ -0,0 +1,15 @@
+﻿using Prion.Application.Timing;
+
+namespace Vitaru.Editor
+{
+    public class SeekableClock : AtomicClock
+    {
+        public override double Current => base.Current - seekOffset;
+
+        public override double LastCurrent => base.LastCurrent - seekOffset;
+
+        private double seekOffset;
+
+        public void Seek(double time) => seekOffset = base.Current - time;
+    }
+}
