1f5a3f7c31fdc57529dc05a819cc16617455f39c
Shawdooow <banhammer1915@gmail.com>
2020-05-19 15:48:16 -0400

    Create Modloader.cs

---
+++ Vitaru/Mods/Modloader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vitaru.Mods
+{
+    public static class Modloader
+    {
+        public static void ReloadMods()
+        {
+
+        }
+    }
+}
