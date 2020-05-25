3499f56411e876c55228674ebd03c2dd31128e48
Shawdooow <banhammer1915@gmail.com>
2020-05-24 23:14:54 -0400

    Update ShadeLayer.cs

--- Vitaru/Graphics/ShadeLayer.cs
+++ Vitaru/Graphics/ShadeLayer.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) 2018-2020 Shawn Bozek.
 // Licensed under EULA https://docs.google.com/document/d/1xPyZLRqjLYcKMxXLHLmA5TxHV-xww7mHYVUuWLt2q9g/edit?usp=sharing
 
+using System;
+using Prion.Core.Threads;
 using Prion.Game.Graphics;
 using Prion.Game.Graphics.Drawables;
 using Prion.Game.Graphics.Layers;
@@ -10,6 +12,7 @@ namespace Vitaru.Graphics
     public class ShadeLayer<D> : Layer2D<D>
         where D : IDrawable2D
     {
+        [ThreadSaftey(Threads.All)]
         public virtual Shades Shade { get; set; }
 
         public override void Render()
