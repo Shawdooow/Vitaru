71966529299d3f8e257041bfd1e7d806a3bafe41
Shawdooow <banhammer1915@gmail.com>
2020-09-01 21:56:01 -0400

    set level data with a button on blank levels

--- Vitaru/Editor/UI/LevelProperties.cs
+++ Vitaru/Editor/UI/LevelProperties.cs
@@ -1,12 +1,14 @@
 ﻿// Copyright (c) 2018-2020 Shawn Bozek.
 // Licensed under EULA https://docs.google.com/document/d/1xPyZLRqjLYcKMxXLHLmA5TxHV-xww7mHYVUuWLt2q9g/edit?usp=sharing
 
+using System;
 using System.Drawing;
 using System.Numerics;
 using Prion.Mitochondria.Graphics.Drawables;
 using Prion.Mitochondria.Graphics.Layers;
 using Prion.Mitochondria.Graphics.Sprites;
 using Prion.Mitochondria.Graphics.Text;
+using Prion.Mitochondria.Graphics.UI;
 using Vitaru.Levels;
 using Vitaru.Themes;
 
@@ -17,10 +19,18 @@ namespace Vitaru.Editor.UI
         private const float width = 300;
         private const float height = 600;
 
+        private readonly ListLayer<IDrawable2D> list;
+
+        private readonly TextBox title;
+        private readonly TextBox artist;
+        private readonly TextBox creator;
+        private readonly TextBox level;
+
+        public Action OnCreate;
+
         public LevelProperties()
         {
             Alpha = 0;
-            PassDownInput = false;
 
             ParentOrigin = Mounts.Center;
             Origin = Mounts.Center;
@@ -36,7 +46,7 @@ namespace Vitaru.Editor.UI
                     Size = new Vector2(width, height),
                     Color = Color.Black
                 },
-                new ListLayer<IDrawable2D>
+                list = new ListLayer<IDrawable2D>
                 {
                     Size = new Vector2(width, height),
 
@@ -75,7 +85,7 @@ namespace Vitaru.Editor.UI
                             Text = "Title",
                             Color = ThemeManager.PrimaryColor,
                         },
-                        new TextBox
+                        title = new TextBox
                         {
                             SpriteText =
                             {
@@ -103,7 +113,7 @@ namespace Vitaru.Editor.UI
                             Text = "Artist",
                             Color = ThemeManager.PrimaryColor,
                         },
-                        new TextBox
+                        artist = new TextBox
                         {
                             SpriteText =
                             {
@@ -131,7 +141,7 @@ namespace Vitaru.Editor.UI
                             Text = "Creator (You)",
                             Color = ThemeManager.PrimaryColor,
                         },
-                        new TextBox
+                        creator = new TextBox
                         {
                             SpriteText =
                             {
@@ -159,7 +169,7 @@ namespace Vitaru.Editor.UI
                             Text = "Level Name",
                             Color = ThemeManager.PrimaryColor,
                         },
-                        new TextBox
+                        level = new TextBox
                         {
                             SpriteText =
                             {
@@ -171,9 +181,56 @@ namespace Vitaru.Editor.UI
                             Origin = Mounts.TopCenter,
                             Text = LevelStore.CurrentLevel.LevelName
                         },
+                        new Box
+                        {
+                            Name = "Spacer",
+                            ParentOrigin = Mounts.TopCenter,
+                            Origin = Mounts.TopCenter,
+                            Size = new Vector2(width - 20, 2),
+                            Color = ThemeManager.SecondaryColor,
+                        },
+
+                        new Button
+                        {
+                            Name = "Create",
+                            ParentOrigin = Mounts.TopCenter,
+                            Origin = Mounts.TopCenter,
+                            Size = new Vector2(width - 20, 40),
+
+                            BackgroundSprite =
+                            {
+                                Color = ThemeManager.PrimaryColor,
+                            },
+
+                            Text = "Create",
+                            OnClick = create
+                        },
                     }
                 },
             };
+        }
+
+        public override void LoadingComplete()
+        {
+            base.LoadingComplete();
+            PassDownInput = false;
+        }
+
+        private void create()
+        {
+            //Check to make sure we got good inputs
+            foreach (IDrawable2D drawable in list)
+                if (drawable is TextBox text && text.Text == null)
+                    return;
+
+            LevelStore.CurrentLevel.Format = LevelStore.VERSION_ONE;
+
+            LevelStore.CurrentLevel.LevelTrack.Title = title.Text;
+            LevelStore.CurrentLevel.LevelTrack.Artist = artist.Text;
+            LevelStore.CurrentLevel.LevelCreator = creator.Text;
+            LevelStore.CurrentLevel.LevelName = level.Text;
+
+            OnCreate?.Invoke();
         }
     }
 }
--- Vitaru/Roots/EditorRoot.cs
+++ Vitaru/Roots/EditorRoot.cs
@@ -1,11 +1,14 @@
 ﻿// Copyright (c) 2018-2020 Shawn Bozek.
 // Licensed under EULA https://docs.google.com/document/d/1xPyZLRqjLYcKMxXLHLmA5TxHV-xww7mHYVUuWLt2q9g/edit?usp=sharing
 
+using System;
 using System.Drawing;
 using System.Numerics;
 using Prion.Mitochondria;
 using Prion.Mitochondria.Graphics.Layers;
 using Prion.Mitochondria.Graphics.Sprites;
+using Prion.Mitochondria.Graphics.Transforms;
+using Prion.Nucleus.Entitys;
 using Vitaru.Editor.Editables;
 using Vitaru.Editor.UI;
 using Vitaru.Levels;
@@ -18,19 +21,21 @@ namespace Vitaru.Roots
 
         protected override bool UseLevelBackground => true;
 
-        private readonly Editfield editfield;
+        private Editfield editfield;
 
         private Timeline timeline;
         private readonly LevelProperties levelProperties;
-        private readonly EditableProperties editableProperties;
+        private EditableProperties editableProperties;
+
+        //bool to queue loading editor on draw thread, gets set by a button (the update thread)
+        private LoadState state;
 
         public EditorRoot()
         {
-            levelProperties = new LevelProperties();
-            if (LevelStore.CurrentPack.Levels[0].Format == LevelStore.BLANK_LEVEL) return;
-
-            editfield = new Editfield();
-            editableProperties = new EditableProperties();
+            levelProperties = new LevelProperties
+            {
+                OnCreate = create
+            };
         }
 
         public override void LoadingComplete()
@@ -44,8 +49,19 @@ namespace Vitaru.Roots
                 base.LoadingComplete();
                 return;
             }
+                
+            loadLevelEditor();
 
+            base.LoadingComplete();
+        }
+
+        private void loadLevelEditor()
+        {
+            //TODO: get around this...
             Game.TextureStore.GetTexture("Edit\\enemyOutline.png");
+
+            editfield = new Editfield();
+            editableProperties = new EditableProperties();
 
             Add(new SpriteLayer
             {
@@ -78,7 +94,21 @@ namespace Vitaru.Roots
             });
             Add(editableProperties);
 
-            base.LoadingComplete();
+            state = LoadState.Loaded;
+
+            //This is a thicc hack, but it works so fine for now
+            if (Cursor == null) return;
+
+            //move the cursor to be on top of the editor...
+            Remove(Cursor, false);
+            Add(Cursor); }
+
+        private void create()
+        {
+            levelProperties.FadeTo(0, 400);
+            levelProperties.PassDownInput = false;
+
+            state = LoadState.PreLoaded;
         }
 
         protected void Selected(Editable editable)
@@ -91,6 +121,14 @@ namespace Vitaru.Roots
         {
             base.Update();
             timeline?.Update();
+        }
+
+        public override void PreRender()
+        {
+            if (state == LoadState.PreLoaded)
+                loadLevelEditor();
+
+            base.PreRender();
         }
     }
 }
