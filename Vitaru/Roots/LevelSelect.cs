490f3ebebe75d79db8ede75a4ddf17a47b54095f
Shawdooow <banhammer1915@gmail.com>
2020-10-21 19:46:01 -0400

    Create LevelSelect.cs

---
+++ Vitaru/Roots/LevelSelect.cs
@@ -0,0 +1,9 @@
+﻿namespace Vitaru.Roots
+{
+    public class LevelSelect : MenuRoot
+    {
+        public override string Name => nameof(LevelSelect);
+
+        protected override bool UseLevelBackground => true;
+    }
+}
