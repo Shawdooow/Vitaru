4ba8f0fed1d77ce8b281b47a1a8d864df22980a0
Shawdooow <banhammer1915@gmail.com>
2021-01-04 10:20:50 -0500

    Update TestMenu.cs

--- Vitaru/Roots/Tests/TestMenu.cs
+++ Vitaru/Roots/Tests/TestMenu.cs
@@ -117,7 +117,7 @@ namespace Vitaru.Roots.Tests
                 ParentOrigin = Mounts.BottomCenter,
                 Origin = Mounts.BottomCenter,
                 FontScale = 0.25f,
-                Text = "0.11.0",
+                Text = "0.11.2",
                 Color = Color.LimeGreen
             });
 
