fb8015706147eef55b98c200d2187d09eb2e2479
Shawdooow <banhammer1915@gmail.com>
2021-06-24 22:15:28 -0400

    Update JoinMatchPacket.cs

--- Vitaru.Server/Packets/Lobby/JoinMatchPacket.cs
+++ Vitaru.Server/Packets/Lobby/JoinMatchPacket.cs
@@ -2,7 +2,9 @@
 // Licensed under EULA https://docs.google.com/document/d/1xPyZLRqjLYcKMxXLHLmA5TxHV-xww7mHYVUuWLt2q9g/edit?usp=sharing
 
 using System;
+using System.Collections.Generic;
 using Prion.Centrosome.Packets.Types;
+using Prion.Nucleus.Utilities;
 using Vitaru.Server.Server;
 
 namespace Vitaru.Server.Packets.Lobby
@@ -17,19 +19,51 @@ namespace Vitaru.Server.Packets.Lobby
         {
         }
 
-        public override IPacket Copy()
-        {
-            throw new NotImplementedException();
-        }
+        public override IPacket Copy() => new JoinMatchPacket();
 
         public override byte[] Serialize()
         {
-            throw new NotImplementedException();
+            List<byte> data = new();
+
+            byte[] match = BitConverter.GetBytes(Match);
+            byte[] user = User.Serialize();
+
+            data.AddRange(match);
+            data.AddRange(user);
+
+            //last we stick the size in.
+            //while it is technically possible to deduce the size of the data on the other side it is wildly impractical to implement
+            byte[] size = BitConverter.GetBytes(data.Count);
+            data.InsertRange(0, size);
+
+            return data.ToArray();
         }
 
         public override void DeSerialize(byte[] data)
         {
-            throw new NotImplementedException();
+            int offset = 0;
+
+            //start with match
+            byte[] length = data.SubArray(offset, 4);
+            offset += length.Length;
+            int size = BitConverter.ToInt32(length);
+
+            byte[] match = data.SubArray(offset, size);
+            offset += match.Length;
+
+            Match = BitConverter.ToInt64(match);
+
+            //now do the user
+            length = data.SubArray(offset, 4);
+            offset += length.Length;
+            size = BitConverter.ToInt32(length);
+
+            byte[] user = data.SubArray(offset, size);
+            offset += user.Length;
+
+            VitaruUser u = new();
+            u.DeSerialize(user);
+            User = u;
         }
     }
 }
