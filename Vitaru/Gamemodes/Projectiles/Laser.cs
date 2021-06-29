2df51ee904c88004728ac2b01a88bd3834e5f491
Shawdooow <banhammer1915@gmail.com>
2021-06-29 14:58:45 -0400

    Create Laser.cs

---
+++ Vitaru/Gamemodes/Projectiles/Laser.cs
@@ -0,0 +1,24 @@
+﻿using Vitaru.Settings;
+
+namespace Vitaru.Gamemodes.Projectiles
+{
+    public class Laser : Projectile
+    {
+        public static int COUNT;
+
+        private readonly bool particles = global::Vitaru.Vitaru.VitaruSettings.GetBool(VitaruSetting.Particles);
+
+        private readonly float particles_multiplier =
+            global::Vitaru.Vitaru.VitaruSettings.GetFloat(VitaruSetting.ParticleMultiplier);
+
+        public override string Name { get; set; } = nameof(Laser);
+
+        public override DrawableGameEntity GenerateDrawable() => null;
+
+        protected override void Dispose(bool finalize)
+        {
+            base.Dispose(finalize);
+            COUNT--;
+        }
+    }
+}
