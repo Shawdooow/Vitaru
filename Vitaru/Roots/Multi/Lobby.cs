dfd9fbedd8fcfb8862c9419ddd3b738e10b5bce0
Shawdooow <banhammer1915@gmail.com>
2021-07-11 09:41:39 -0400

    ensure packets are being sent to lobby root

--- Vitaru/Roots/Multi/Lobby.cs
+++ Vitaru/Roots/Multi/Lobby.cs
@@ -2,6 +2,7 @@
 // Licensed under EULA https://docs.google.com/document/d/1xPyZLRqjLYcKMxXLHLmA5TxHV-xww7mHYVUuWLt2q9g/edit?usp=sharing
 
 using System.Numerics;
+using Prion.Centrosome.NetworkingHandlers;
 using Prion.Golgi.Audio.Tracks;
 using Prion.Mitochondria.Graphics.Drawables;
 using Prion.Mitochondria.Graphics.Layers;
@@ -11,6 +12,7 @@ using Prion.Mitochondria.Graphics.UI;
 using Prion.Nucleus.Entitys;
 using Prion.Nucleus.Groups.Packs;
 using Vitaru.Levels;
+using Vitaru.Networking.Client;
 using Vitaru.Server.Match;
 using Vitaru.Server.Packets.Lobby;
 
@@ -20,7 +22,9 @@ namespace Vitaru.Roots.Multi
     {
         protected override bool Parallax => true;
 
-        private TrackController controller;
+        private readonly Rooms rooms;
+
+        private readonly TrackController controller;
 
         public Lobby(Pack<Updatable> networking) : base(networking)
         {
@@ -34,7 +38,7 @@ namespace Vitaru.Roots.Multi
                     Position = new Vector2(10),
                     Text = "Rooms:"
                 },
-                new Rooms(),
+                rooms = new Rooms(),
                 new Button
                 {
                     ParentOrigin = Mounts.BottomLeft,
@@ -79,6 +83,18 @@ namespace Vitaru.Roots.Multi
             controller.TryRepeat();
         }
 
+        protected override void OnPacketRecieve(PacketInfo<VitaruHost> info)
+        {
+            base.OnPacketRecieve(info);
+
+            switch (info.Packet)
+            {
+                case MatchListPacket list:
+                    rooms.RefreshRooms(list);
+                    break;
+            }
+        }
+
         private void create()
         {
             VitaruNet.SendPacketTcp(new CreateMatchPacket
@@ -110,6 +126,11 @@ namespace Vitaru.Roots.Multi
         public class Rooms : InputLayer<IDrawable2D>
         {
             public Rooms()
+            {
+
+            }
+
+            public void RefreshRooms(MatchListPacket list)
             {
 
             }
