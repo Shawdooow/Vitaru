385755cea6121b9f9881df4be4222039243c0289
Shawdooow <banhammer1915@gmail.com>
2021-07-20 21:04:44 -0400

    Create MatchRoot.cs

---
+++ Vitaru/Roots/Multi/MatchRoot.cs
@@ -0,0 +1,13 @@
+﻿using Prion.Nucleus.Entitys;
+using Prion.Nucleus.Groups.Packs;
+using Vitaru.Server.Match;
+
+namespace Vitaru.Roots.Multi
+{
+    public class MatchRoot : MultiRoot
+    {
+        public MatchRoot(MatchInfo match, Pack<Updatable> networking) : base(networking)
+        {
+        }
+    }
+}
