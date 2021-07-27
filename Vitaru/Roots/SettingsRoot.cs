bded6a6220b8dd6e1e9d9729d7557adc4ddd4f9a
Shawdooow <banhammer1915@gmail.com>
2021-07-27 15:13:46 -0400

    Create SettingsRoot.cs

---
+++ Vitaru/Roots/SettingsRoot.cs
@@ -0,0 +1,18 @@
+﻿using Vitaru.Roots.Menu;
+
+namespace Vitaru.Roots
+{
+    public class SettingsRoot : MenuRoot
+    {
+        public override string Name => nameof(SettingsRoot);
+
+        protected override bool UseLevelBackground => true;
+
+        protected override bool Parallax => true;
+
+        public SettingsRoot()
+        {
+            Add(new Version());
+        }
+    }
+}
