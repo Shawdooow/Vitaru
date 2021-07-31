e92eff11bf7a28c3996ce34f95270b92bc97b4f5
Shawdooow <banhammer1915@gmail.com>
2021-07-30 21:58:12 -0400

    move Frost's ability to Yuie

--- Vitaru/Gamemodes/Vitaru/Chapters/Alki/Three/Frost.cs
+++ Vitaru/Gamemodes/Vitaru/Chapters/Alki/Three/Frost.cs
@@ -1,13 +1,8 @@
 ﻿// Copyright (c) 2018-2021 Shawn Bozek.
 // Licensed under EULA https://docs.google.com/document/d/1xPyZLRqjLYcKMxXLHLmA5TxHV-xww7mHYVUuWLt2q9g/edit?usp=sharing
 
-using System;
 using System.Drawing;
-using Prion.Golgi.Audio.Tracks;
-using Prion.Nucleus.Debug;
-using Prion.Nucleus.Timing;
 using Prion.Nucleus.Utilities;
-using Vitaru.Input;
 using Vitaru.Play;
 using Vitaru.Play.Characters.Players;
 
@@ -19,13 +14,13 @@ namespace Vitaru.Gamemodes.Vitaru.Chapters.Alki.Three
 
         public override string Name => "Frost Pine";
 
-        public override float HealthCapacity => 60;
+        //public override float HealthCapacity => 60;
 
-        public override float EnergyCapacity => 24;
+        //public override float EnergyCapacity => 24;
 
-        public override float EnergyCost => 4;
+        //public override float EnergyCost => 4;
 
-        public override float EnergyDrainRate => 4;
+        //public override float EnergyDrainRate => 4;
 
         public override Color PrimaryColor => "#a1e4ff".HexToColor();
 
@@ -33,180 +28,18 @@ namespace Vitaru.Gamemodes.Vitaru.Chapters.Alki.Three
 
         public override Color ComplementaryColor => "#c2c2c2".HexToColor();
 
-        public override string Ability => "Time Traveler";
+        //public override string Ability => "Time Traveler";
 
-        public override Role Role => Role.Defense;
+        //public override Role Role => Role.Defense;
 
-        public override Difficulty Difficulty => Difficulty.Normal;
+        //public override Difficulty Difficulty => Difficulty.Normal;
 
-        public override bool Implemented => true;
-
-        public double SetRate { get; private set; } = 0.75d;
-
-        private double originalRate;
-
-        private double currentRate = 1;
-
-        private double spellEndTime { get; set; } = double.MinValue;
-
-        private AdjustableClock adjustable;
+        public override bool Implemented => false;
 
         #endregion
 
         public Frost(Gamefield gamefield) : base(gamefield)
         {
-        }
-
-        public override void LoadingComplete()
-        {
-            base.LoadingComplete();
-            if (Clock is AdjustableClock a)
-                adjustable = a;
-            else
-                Debugger.InvalidOperation($"{nameof(Frost)} requires an {nameof(AdjustableClock)} as it's {nameof(Clock)}!");
-        }
-
-        protected override bool CheckSpellActivate(VitaruActions action)
-        {
-            if (action == VitaruActions.ModifierOne)
-                return true;
-            if (action == VitaruActions.ModifierTwo)
-                return true;
-
-            return base.CheckSpellActivate(action);
-        }
-
-        protected override void SpellActivate(VitaruActions action)
-        {
-            if (action == VitaruActions.ModifierOne)
-            {
-                SetRate = Math.Min(
-                    Binds[VitaruActions.Sneak]
-                        ? Math.Round(SetRate + 0.05d, 2)
-                        : Math.Round(SetRate + 0.25d, 2), 2d);
-                intensity();
-                return;
-            }
-
-            if (action == VitaruActions.ModifierTwo)
-            {
-                SetRate = Math.Max(
-                    Binds[VitaruActions.Sneak]
-                        ? Math.Round(SetRate - 0.05d, 2)
-                        : Math.Round(SetRate - 0.25d, 2), -2d);
-                intensity();
-                return;
-            }
-
-            base.SpellActivate(action);
-
-            if (originalRate == 0)
-                originalRate = (float) adjustable.Rate;
-
-            currentRate = originalRate * SetRate;
-            applyToClock(adjustable, currentRate);
-
-            if (currentRate > 0)
-                spellEndTime = Clock.LastCurrent + 2000;
-            else if (currentRate == 0)
-                spellEndTime = Clock.LastCurrent;
-            else
-                spellEndTime = Clock.LastCurrent - 2000;
-
-            intensity();
-            Gamefield.Shade = Shades.Blue;
-            DrawablePlayer.Sprite.Color = Color.Blue;
-            DrawablePlayer.HitboxOutline.Color = Color.Blue;
-            DrawablePlayer.Seal.Reticle.Color = Color.GhostWhite;
-            DrawablePlayer.Seal.Sign.Color = Color.GhostWhite;
-        }
-
-        protected override void SpellDeactivate(VitaruActions action)
-        {
-            base.SpellDeactivate(action);
-
-            DrawablePlayer.Sprite.Color = PrimaryColor;
-            DrawablePlayer.HitboxOutline.Color = PrimaryColor;
-            DrawablePlayer.Seal.Reticle.Color = PrimaryColor;
-            DrawablePlayer.Seal.Sign.Color = PrimaryColor;
-        }
-
-        private void intensity()
-        {
-            float scale = (float) Easing.ApplyEasing(Easings.OutQuad,
-                Math.Min(PrionMath.Remap(currentRate, 1d, currentRate > 1d ? 2d : 0.5d), 1d));
-            Gamefield.Intensity = scale;
-        }
-
-        protected override void SpellUpdate()
-        {
-            base.SpellUpdate();
-
-            if (spellEndTime >= Clock.LastCurrent && currentRate > 0 ||
-                spellEndTime == Clock.LastCurrent && currentRate == 0 ||
-                spellEndTime <= Clock.LastCurrent && currentRate < 0)
-                if (!SpellActive)
-                {
-                    currentRate += (float) Clock.LastElapsedTime / 200;
-
-                    if (currentRate > originalRate || currentRate <= 0)
-                        currentRate = originalRate;
-
-                    applyToClock(adjustable, currentRate);
-
-                    if (currentRate > 0 && spellEndTime - 1000 <= Clock.LastCurrent ||
-                        currentRate < 0 && spellEndTime + 1000 >= Clock.LastCurrent)
-                    {
-                        currentRate = originalRate;
-                        applyToClock(adjustable, currentRate);
-
-                        Gamefield.Shade = Shades.None;
-                    }
-
-                    intensity();
-                }
-                else
-                {
-                    double energyDrainMultiplier = 0;
-
-                    if (currentRate < 1)
-                        energyDrainMultiplier = originalRate - currentRate;
-                    else if (currentRate >= 1)
-                        energyDrainMultiplier = currentRate - originalRate;
-
-                    DrainEnergy((float) Clock.LastElapsedTime / 1000f *
-                                (1f / (float) currentRate * (float) energyDrainMultiplier * EnergyDrainRate +
-                                 EnergyCost));
-
-                    if (currentRate > 0)
-                        spellEndTime = Clock.LastCurrent + 2000;
-                    else if (currentRate == 0)
-                        spellEndTime = Clock.LastCurrent;
-                    else
-                        spellEndTime = Clock.LastCurrent - 2000;
-
-                    currentRate = originalRate * SetRate;
-                    applyToClock(adjustable, currentRate);
-                    intensity();
-                }
-
-            if (DrawablePlayer != null) DrawablePlayer.Seal.LeftValue.Text = $"{SetRate}x";
-        }
-
-        private void applyToClock(AdjustableClock clock, double speed)
-        {
-            if (TrackManager.CurrentTrack != null)
-                TrackManager.CurrentTrack.Pitch = (float) speed;
-            clock.Rate = speed;
-
-            MovementSpeedMultiplier = 1 / speed;
-        }
-
-        protected override void BulletAddRad(float speed, float angle, Color color, float size, float damage,
-            float distance)
-        {
-            if (SpellActive && color == PrimaryColor) color = Color.Blue;
-            base.BulletAddRad(speed, angle, color, size, damage, distance);
         }
     }
 }
--- Vitaru/Gamemodes/Vitaru/Chapters/Alki/Two/Yuie.cs
+++ Vitaru/Gamemodes/Vitaru/Chapters/Alki/Two/Yuie.cs
@@ -1,8 +1,13 @@
 ﻿// Copyright (c) 2018-2021 Shawn Bozek.
 // Licensed under EULA https://docs.google.com/document/d/1xPyZLRqjLYcKMxXLHLmA5TxHV-xww7mHYVUuWLt2q9g/edit?usp=sharing
 
+using System;
 using System.Drawing;
+using Prion.Golgi.Audio.Tracks;
+using Prion.Nucleus.Debug;
+using Prion.Nucleus.Timing;
 using Prion.Nucleus.Utilities;
+using Vitaru.Input;
 using Vitaru.Play;
 using Vitaru.Play.Characters.Players;
 
@@ -16,9 +21,11 @@ namespace Vitaru.Gamemodes.Vitaru.Chapters.Alki.Two
 
         public override float HealthCapacity => 60;
 
-        public override float EnergyCapacity => 32;
+        public override float EnergyCapacity => 24;
 
-        public override float EnergyCost => 16;
+        public override float EnergyCost => 4;
+
+        public override float EnergyDrainRate => 4;
 
         public override Color PrimaryColor => "#00ffbb".HexToColor();
 
@@ -26,16 +33,180 @@ namespace Vitaru.Gamemodes.Vitaru.Chapters.Alki.Two
 
         public override Color ComplementaryColor => "#3d2a69".HexToColor();
 
-        public override string Ability => "Snap Shot";
+        public override string Ability => "Time Traveler";
 
-        public override Role Role => Role.Support;
+        public override Role Role => Role.Defense;
 
         public override Difficulty Difficulty => Difficulty.Normal;
+
+        public override bool Implemented => true;
+
+        public double SetRate { get; private set; } = 0.75d;
+
+        private double originalRate;
+
+        private double currentRate = 1;
+
+        private double spellEndTime { get; set; } = double.MinValue;
+
+        private AdjustableClock adjustable;
 
         #endregion
 
         public Yuie(Gamefield gamefield) : base(gamefield)
         {
+        }
+
+        public override void LoadingComplete()
+        {
+            base.LoadingComplete();
+            if (Clock is AdjustableClock a)
+                adjustable = a;
+            else
+                Debugger.InvalidOperation($"{nameof(Yuie)} requires an {nameof(AdjustableClock)} as it's {nameof(Clock)}!");
+        }
+
+        protected override bool CheckSpellActivate(VitaruActions action)
+        {
+            if (action == VitaruActions.ModifierOne)
+                return true;
+            if (action == VitaruActions.ModifierTwo)
+                return true;
+
+            return base.CheckSpellActivate(action);
+        }
+
+        protected override void SpellActivate(VitaruActions action)
+        {
+            if (action == VitaruActions.ModifierOne)
+            {
+                SetRate = Math.Min(
+                    Binds[VitaruActions.Sneak]
+                        ? Math.Round(SetRate + 0.05d, 2)
+                        : Math.Round(SetRate + 0.25d, 2), 2d);
+                intensity();
+                return;
+            }
+
+            if (action == VitaruActions.ModifierTwo)
+            {
+                SetRate = Math.Max(
+                    Binds[VitaruActions.Sneak]
+                        ? Math.Round(SetRate - 0.05d, 2)
+                        : Math.Round(SetRate - 0.25d, 2), -2d);
+                intensity();
+                return;
+            }
+
+            base.SpellActivate(action);
+
+            if (originalRate == 0)
+                originalRate = (float)adjustable.Rate;
+
+            currentRate = originalRate * SetRate;
+            applyToClock(adjustable, currentRate);
+
+            if (currentRate > 0)
+                spellEndTime = Clock.LastCurrent + 2000;
+            else if (currentRate == 0)
+                spellEndTime = Clock.LastCurrent;
+            else
+                spellEndTime = Clock.LastCurrent - 2000;
+
+            intensity();
+            Gamefield.Shade = Shades.Blue;
+            DrawablePlayer.Sprite.Color = ComplementaryColor;
+            DrawablePlayer.HitboxOutline.Color = ComplementaryColor;
+            DrawablePlayer.Seal.Reticle.Color = SecondaryColor;
+            DrawablePlayer.Seal.Sign.Color = SecondaryColor;
+        }
+
+        protected override void SpellDeactivate(VitaruActions action)
+        {
+            base.SpellDeactivate(action);
+
+            DrawablePlayer.Sprite.Color = PrimaryColor;
+            DrawablePlayer.HitboxOutline.Color = PrimaryColor;
+            DrawablePlayer.Seal.Reticle.Color = PrimaryColor;
+            DrawablePlayer.Seal.Sign.Color = PrimaryColor;
+        }
+
+        private void intensity()
+        {
+            float scale = (float)Easing.ApplyEasing(Easings.OutQuad,
+                Math.Min(PrionMath.Remap(currentRate, 1d, currentRate > 1d ? 2d : 0.5d), 1d));
+            Gamefield.Intensity = scale;
+        }
+
+        protected override void SpellUpdate()
+        {
+            base.SpellUpdate();
+
+            if (spellEndTime >= Clock.LastCurrent && currentRate > 0 ||
+                spellEndTime == Clock.LastCurrent && currentRate == 0 ||
+                spellEndTime <= Clock.LastCurrent && currentRate < 0)
+                if (!SpellActive)
+                {
+                    currentRate += (float)Clock.LastElapsedTime / 200;
+
+                    if (currentRate > originalRate || currentRate <= 0)
+                        currentRate = originalRate;
+
+                    applyToClock(adjustable, currentRate);
+
+                    if (currentRate > 0 && spellEndTime - 1000 <= Clock.LastCurrent ||
+                        currentRate < 0 && spellEndTime + 1000 >= Clock.LastCurrent)
+                    {
+                        currentRate = originalRate;
+                        applyToClock(adjustable, currentRate);
+
+                        Gamefield.Shade = Shades.None;
+                    }
+
+                    intensity();
+                }
+                else
+                {
+                    double energyDrainMultiplier = 0;
+
+                    if (currentRate < 1)
+                        energyDrainMultiplier = originalRate - currentRate;
+                    else if (currentRate >= 1)
+                        energyDrainMultiplier = currentRate - originalRate;
+
+                    DrainEnergy((float)Clock.LastElapsedTime / 1000f *
+                                (1f / (float)currentRate * (float)energyDrainMultiplier * EnergyDrainRate +
+                                 EnergyCost));
+
+                    if (currentRate > 0)
+                        spellEndTime = Clock.LastCurrent + 2000;
+                    else if (currentRate == 0)
+                        spellEndTime = Clock.LastCurrent;
+                    else
+                        spellEndTime = Clock.LastCurrent - 2000;
+
+                    currentRate = originalRate * SetRate;
+                    applyToClock(adjustable, currentRate);
+                    intensity();
+                }
+
+            if (DrawablePlayer != null) DrawablePlayer.Seal.LeftValue.Text = $"{SetRate}x";
+        }
+
+        private void applyToClock(AdjustableClock clock, double speed)
+        {
+            if (TrackManager.CurrentTrack != null)
+                TrackManager.CurrentTrack.Pitch = (float)speed;
+            clock.Rate = speed;
+
+            MovementSpeedMultiplier = 1 / speed;
+        }
+
+        protected override void BulletAddRad(float speed, float angle, Color color, float size, float damage,
+            float distance)
+        {
+            if (SpellActive && color == PrimaryColor) color = ComplementaryColor;
+            base.BulletAddRad(speed, angle, color, size, damage, distance);
         }
     }
 }
