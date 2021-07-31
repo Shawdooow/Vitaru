b058c77a6db914b7d5f77b07fc7b6c3545ce7ea1
Shawdooow <banhammer1915@gmail.com>
2021-07-31 19:53:46 -0400

    version text

--- Vitaru.Server/VitaruServer.cs
+++ Vitaru.Server/VitaruServer.cs
@@ -11,7 +11,7 @@ namespace Vitaru.Server
 {
     public class VitaruServer : Application
     {
-        public const string VERSION = "0.12.0-preview3.0";
+        public const string VERSION = "0.12.0-preview3.2";
 
         private const string host =
 #if true
