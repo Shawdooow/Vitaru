3c24d1dcb1d0d01d704c5bbaa8f32e09d6b5d03e
Shawdooow <banhammer1915@gmail.com>
2021-08-19 20:44:50 -0400

    BAD USINGS

--- Vitaru/Play/Characters/Players/Player.cs
+++ Vitaru/Play/Characters/Players/Player.cs
@@ -4,11 +4,9 @@
 using System.Drawing;
 using System.Numerics;
 using Prion.Golgi.Audio.Tracks;
-using Prion.Mitochondria;
 using Prion.Mitochondria.Audio;
 using Prion.Mitochondria.Graphics;
 using Prion.Mitochondria.Graphics.Drawables;
-using Prion.Mitochondria.Graphics.Sprites;
 using Prion.Mitochondria.Input;
 using Prion.Nucleus;
 using Prion.Nucleus.Utilities;
