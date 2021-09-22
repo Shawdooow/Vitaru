a1b87cfdb64b1827e049e61ce19f518b59d557de
Shawdooow <banhammer1915@gmail.com>
2021-09-22 08:55:46 -0400

    Update Vitaru.cs

--- Vitaru/Vitaru.cs
+++ Vitaru/Vitaru.cs
@@ -117,7 +117,7 @@ namespace Vitaru
             vitaru.Start(new MainMenu(vitaru));
         }
 
-        public static bool EnableCharacters => FEATURES >= Features.Radioactive;
+        public static bool EnableCharacters => FEATURES >= Features.Experimental;
         public static bool EnableTanks => FEATURES >= Features.Upcoming;
         public static bool EnableMulti => FEATURES >= Features.Radioactive;
 
