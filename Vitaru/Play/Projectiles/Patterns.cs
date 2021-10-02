91bb80c89a7da342eb14f547ec96edf9104909fb
Shawdooow <banhammer1915@gmail.com>
2021-10-02 17:26:25 -0400

    Update Patterns.cs

--- Vitaru/Play/Projectiles/Patterns.cs
+++ Vitaru/Play/Projectiles/Patterns.cs
@@ -318,7 +318,7 @@ namespace Vitaru.Play.Projectiles
                     direction += MathF.PI / (arms / 2f);
                 }
 
-                direction += MathF.PI / 15.5f;
+                direction += MathF.PI / 0.95f;
             }
 
             return projectiles;
