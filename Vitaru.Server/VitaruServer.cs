e0050c9577878c5b80f177537fdd2cb389638e94
Shawdooow <banhammer1915@gmail.com>
2021-10-28 17:40:54 -0400

    Update VitaruServer.cs

--- Vitaru.Server/VitaruServer.cs
+++ Vitaru.Server/VitaruServer.cs
@@ -11,7 +11,7 @@ namespace Vitaru.Server
 {
     public class VitaruServer : Application
     {
-        public const string VERSION = "0.12.0-preview4.1";
+        public const string VERSION = "0.12.0-preview4.2";
 
         private const string host = "VitaruServer";
 
