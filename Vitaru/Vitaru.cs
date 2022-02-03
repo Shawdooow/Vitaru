166558d836d0008a025d900264dff25f92e4a56d
Shawdooow <banhammer1915@gmail.com>
2022-02-03 17:46:10 -0500

    Update Vitaru.cs

--- Vitaru/Vitaru.cs
+++ Vitaru/Vitaru.cs
@@ -95,6 +95,9 @@ namespace Vitaru
                     //if (somber && ThemeManager.Theme.Name == Ecstatic.NAME)
                     //    ThemeManager.Theme = new Somber();
                     break;
+                //case 4 when DateTime.Now.Day == 4:
+                    //ThemeManager.Theme = new Murica();
+                    //break;
                 case 10:
                     ThemeManager.Theme = new Spooky();
                     break;
