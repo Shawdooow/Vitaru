bc6a0d934cb7cf69ec73319ca4b267c7882617b0
Shawdooow <banhammer1915@gmail.com>
2022-02-07 17:52:58 -0500

    add metronome for muris

---
+++ Vitaru/Gamemodes/Vitaru/Chapters/Abilities/Metronome.cs
@@ -0,0 +1,13 @@
+﻿using Prion.Mitochondria.Graphics.Drawables;
+using Prion.Mitochondria.Graphics.Layers._2D;
+using Vitaru.Play.Characters.Players;
+
+namespace Vitaru.Gamemodes.Vitaru.Chapters.Abilities
+{
+    public class Metronome : Layer2D<IDrawable2D>
+    {
+        public Metronome(Player player, Layer2D<IDrawable2D> overlays)
+        {
+        }
+    }
+}
