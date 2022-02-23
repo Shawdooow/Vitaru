4eff63aa9e992fa8c961f9d7197cbf2d9df3aa22
Shawdooow <banhammer1915@gmail.com>
2022-02-23 14:32:43 -0500

    Update Patterns.cs

--- Vitaru/Play/Projectiles/Patterns.cs
+++ Vitaru/Play/Projectiles/Patterns.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Numerics;
 using Prion.Nucleus.Utilities;
+using Vitaru.Editor.KeyFrames.Interfaces;
 
 namespace Vitaru.Play.Projectiles
 {
