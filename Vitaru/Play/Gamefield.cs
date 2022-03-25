15bfa46f37eb020bc187e628ca9fcf05dbbad6a9
Shawdooow <banhammer1915@gmail.com>
2022-03-25 15:25:10 -0400

    Update Gamefield.cs

--- Vitaru/Play/Gamefield.cs
+++ Vitaru/Play/Gamefield.cs
@@ -2,12 +2,20 @@
 // Licensed under EULA https://docs.google.com/document/d/1xPyZLRqjLYcKMxXLHLmA5TxHV-xww7mHYVUuWLt2q9g/edit?usp=sharing
 
 using System.Numerics;
+using Prion.Mitochondria.Graphics.Drawables;
 using Prion.Mitochondria.Graphics.Layers._2D;
 using Prion.Mitochondria.Graphics.Sprites;
+using Prion.Nucleus.Debug;
+using Prion.Nucleus.Groups.Packs;
+using Vitaru.Editor.IO;
+using Vitaru.Gamemodes;
+using Vitaru.Levels;
+using Vitaru.Play.Characters;
+using Vitaru.Play.Characters.Players;
 
 namespace Vitaru.Play
 {
-    public class Gamefield
+    public class Gamefield : Pack<IPack>
     {
         public static double Current { get; private set; } = double.MinValue;
 
@@ -15,9 +23,45 @@ namespace Vitaru.Play
 
         public virtual float Intensity { get; set; } = 1;
 
+        protected readonly FormatConverter FormatConverter;
+
+        public readonly Pack<Character> CharacterPack = new()
+        {
+            Name = "Character Pack",
+        };
+
+        public readonly Layer2D<IDrawable2D> CharacterLayer = new()
+        {
+            Name = "Character Layer",
+            Size = GamemodeStore.SelectedGamemode.Gamemode.GetGamefieldSize(),
+        };
+
+        public Player ActivePlayer { get; protected set; }
+
+        public readonly GamefieldBorder Border;
+
         public Gamefield()
         {
+            Current = double.MinValue;
 
+            Vector2 size = GamemodeStore.SelectedGamemode.Gamemode.GetGamefieldSize();
+
+            Add(CharacterPack);
+
+            FormatConverter = GamemodeStore.SelectedGamemode.Gamemode.GetFormatConverter();
+            FormatConverter.Gamefield = this;
+
+            try
+            {
+                //if (LevelStore.CurrentLevel.EnemyData != null)
+                    //UnloadedEnemies.AddRange(FormatConverter.StringToEnemies(LevelStore.CurrentLevel.EnemyData));
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e, "Error converting level data to Enemies, purging bad data...", LogType.IO);
+                //UnloadedEnemies.Clear();
+                LevelStore.CurrentLevel.EnemyData = string.Empty;
+            }
         }
 
         public class GamefieldBorder : Layer2D<Box>
