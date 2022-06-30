991dbed3c1e878cffde7d3c5d3696ac1b5a03445
Shawdooow <banhammer1915@gmail.com>
2022-06-30 09:30:31 -0400

    some new classes

---
+++ Vitaru/Gamemodes/Vitaru/Chapters/Alki/Four/Jaak.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vitaru.Gamemodes.Vitaru.Chapters.Alki.Four
+{
+    internal class Jaak
+    {
+    }
+}
