cda1f3448d73be8c68d6e9605ca5ba9f79531e98
Shawdooow <banhammer1915@gmail.com>
2023-03-19 13:13:30 -0400

    code fixes

---
+++ Vitaru.Mods.Shrekdevor/ShrekStates.cs
@@ -0,0 +1,156 @@
+﻿// Copyright (c) 2018-2023 Shawn Bozek.
+// Licensed under EULA https://docs.google.com/document/d/1xPyZLRqjLYcKMxXLHLmA5TxHV-xww7mHYVUuWLt2q9g/edit?usp=sharing
+
+
+namespace Vitaru.Mods.Shrekdevor
+{
+    public struct ShrekState
+    {
+        public ulong InputState { get; init; }
+        public ulong OutputState { get; init; }
+        public string InputText { get; init; }
+        public string OutputText { get; init; }
+
+
+        internal static ulong State;
+
+
+        private const ulong start = 1;
+
+        private const ulong left = 1000;
+        private const ulong right = 2000;
+
+        private const ulong forward = 3000;
+        private const ulong back = 4000;
+        private const ulong up = 5000;
+        private const ulong down = 6000;
+
+        private const ulong die = 7000;
+
+
+        public static readonly ShrekState[] States = new[]
+        {
+            new ShrekState
+            {
+                InputState = 0,
+                OutputState = start,
+                InputText = "Shrekdevor",
+                OutputText = "Welcome to the Great Shrekdevor REDICED Edition!\n\n" +
+                "You are walking down a path, which direction do you travel? (left, right)",
+            },
+
+
+
+
+            new ShrekState
+            {
+                InputState = start,
+                OutputState = left,
+                InputText = nameof(left),
+                OutputText = "You walk down the left path.\n\n" +
+                "",
+            },
+
+
+            new ShrekState
+            {
+                InputState = start,
+                OutputState = right,
+                InputText = nameof(right),
+                OutputText = "You travel down the right path.\n\n" +
+                "",
+            },
+
+
+            new ShrekState
+            {
+                InputState = start,
+                OutputState = forward,
+                InputText = nameof(forward),
+                OutputText = "Not really caring to make a decision you forage straight forward into the forest.\n\n" +
+                "",
+            },
+
+
+            new ShrekState
+            {
+                InputState = start,
+                OutputState = back,
+                InputText = nameof(back),
+                OutputText = "You begin traveling backwards but fucked up, you are traveling backwards in time.\n\n" +
+                "",
+            },
+
+
+            new ShrekState
+            {
+                InputState = start,
+                OutputState = up,
+                InputText = nameof(up),
+                OutputText = "Despite usually being bound to earth by gravity you manage to start floating straight up.\n\n" +
+                "",
+            },
+
+
+            new ShrekState
+            {
+                InputState = start,
+                OutputState = down,
+                InputText = nameof(down),
+                OutputText = "You begin burrowing into the ground. " +
+                            "A few miles down you turn around to find shrek is behind you and is helping you dig. " +
+                            "You continue until you hit the mantle. " +
+                            "Shrek advises you turn back, what shall you do? (back, cointinue)\n\n" +
+                            "",
+            },
+            new ShrekState
+            {
+                InputState = down,
+                OutputState = down + 100,
+                InputText = "back",
+                OutputText = "You turn back and go to the surface, " +
+                            "only to find that you are no longer on earth but on a giant onion in space with many layers. " +
+                            "Lord fuckwad sent space pirates to space assasinate you, how do you retaliate? (golden ticket, golden onion, railgun, microwave gun)\n\n" +
+                            "",
+            },
+            new ShrekState
+            {
+                InputState = down + 100,
+                OutputState = down + 110,
+                InputText = "golden ticket",
+                OutputText = "You pull a golden ticket out of seemingly nowhere and flail it around. " +
+                            "The polar express appears from behind a asteroid and runs over all the space pirates. It then stops to pick you up, then heads toward the north pole.\n\n" +
+                            "" +
+                            "Once you reach the north pole you realize that all the elfs have been enslaved by Adolf Hitler, " +
+                            "and the nazis are going to board the train and search for more elfs. " +
+                            "You and shrek hop off just outside of town and meet up with the rebels trying to kill Hitler, who are being lead by Santa. " +
+                            "His weapons storehouse is unlike anything you've ever seen; tanks, planes, nukes. " +
+                            "You name it he had it. " +
+                            "His spys inform you all that now is a good time to strike, however Santa wants to perform some ritual before you go. " +
+                            "Whats the plan? (ritual, nuke, sneaky, siege)\n\n" +
+                            "",
+            },
+            new ShrekState
+            {
+                InputState = down + 110,
+                OutputState = down + 111,
+                InputText = "ritual",
+                OutputText = "You perform the ritual by drawing a pentagram on the ground with santa and lighting five candles." +
+                            "Jack Black himself ascends from the depths of hell while absolutely shreading a guitar solo.\n\n" +
+                            "",
+            },
+
+            new ShrekState
+            {
+                InputState = down,
+                OutputState = down + 200,
+                InputText = "continue",
+                OutputText = "You tell shrek you must continue, he understands and follows you down. " +
+                "You continue through the mantle until you reach an opening. " +
+                "You have found Hell. " +
+                "Where do you head next? (lava, center)\n\n" +
+                "",
+            },
+        };
+    }
+}
--- Vitaru.Mods.Shrekdevor/Shrekdevor.cs
+++ Vitaru.Mods.Shrekdevor/Shrekdevor.cs
@@ -11,6 +11,7 @@ using System.Drawing;
 using System.Numerics;
 using Vitaru.Roots;
 using static Prion.Nucleus.Debug.Console;
+using static System.Windows.Forms.AxHost;
 
 namespace Vitaru.Mods.Shrekdevor
 {
@@ -38,24 +39,30 @@ namespace Vitaru.Mods.Shrekdevor
         public override void LoadingComplete()
         {
             base.LoadingComplete();
-            shrek();
-        }
 
-        public static void ShrekState(ulong s)
-        {
-            State = s;
-            Renderer.Console.MessageQueue.Enqueue(new KeyValuePair<string, Color>(GetShrekText(State), Color.Lime));
-        }
+            Commands.Add(new ConsoleCommand("reset") 
+            { 
+                OnCommand = args =>
+                {
+                    ShrekState.State = 0;
+                    Renderer.Console.MessageQueue.Enqueue(new KeyValuePair<string, Color>("Shrekdevor State Reset!", Color.Lime));
+                }
+            });
 
-        public static string GetShrekText(ulong state)
-        {
-            switch (state)
+            for (int i = 0; i < ShrekState.States.Length; i++)
             {
-                default:
-                    return string.Empty;
-                case start:
-                    return "Welcome to the Great Shrekdevor REDICED Edition!\n\n" +
-                            "You are walking down a path, which direction do you travel? (left, right)";
+                ShrekState state = ShrekState.States[i];
+
+                Commands.Add(new ConsoleCommand(state.InputText)
+                {
+                    OnCommand = args =>
+                    {
+                        if (ShrekState.State != state.InputState) return;
+
+                        ShrekState.State = state.OutputState;
+                        Renderer.Console.MessageQueue.Enqueue(new KeyValuePair<string, Color>(state.OutputText, Color.Lime));
+                    }
+                });
             }
         }
 
@@ -72,17 +79,17 @@ namespace Vitaru.Mods.Shrekdevor
             public override void RenderingLoadingComplete()
             {
                 base.RenderingLoadingComplete();
-                SetBackground(State);
+                SetBackground(ShrekState.State);
             }
 
             public override void PreRender()
             {
                 base.PreRender();
 
-                if (State != PreviousState)
+                if (ShrekState.State != PreviousState)
                 {
-                    PreviousState = State;
-                    SetBackground(State);
+                    PreviousState = ShrekState.State;
+                    SetBackground(ShrekState.State);
                 }
             }
 
@@ -97,199 +104,5 @@ namespace Vitaru.Mods.Shrekdevor
                 Background.Texture = Vitaru.TextureStore.GetTexture($"Shrekdevor/{state}/{files[rand]}");
             }
         }
-
-
-        #region Shrekdevor
-
-
-        internal static ulong State;
-
-        private const ulong start = 1;
-
-        private const ulong left = 1000;
-        private const ulong right = 2000;
-
-        private const ulong forward = 3000;
-        private const ulong back = 4000;
-        private const ulong up = 5000;
-        private const ulong down = 6000;
-
-        private const ulong die = 7000;
-
-
-        private void shrek()
-        {
-            Prion.Nucleus.Debug.Console.Commands.AddRange(new[]
-            {
-                //Intro is second to avoid a bug
-                new ConsoleCommand("Shrekdevor", true) { OnCommand = args => ShrekState(start) },
-
-
-
-                //Left
-                new ConsoleCommand("left")
-                {
-                    OnCommand = args =>
-                    {
-                        if (State != start) return;
-                        ShrekState(left);
-                        State = left;
-                        Renderer.Console.MessageQueue.Enqueue(new KeyValuePair<string, Color>("You walk down the left path.\n\n" +
-                            "", Color.Lime));
-                    }
-                },
-
-
-
-                //Right
-                new ConsoleCommand("right")
-                {
-                    OnCommand = args =>
-                    {
-                        if (State != 1) return;
-                        State = right;
-                        Renderer.Console.MessageQueue.Enqueue(new KeyValuePair<string, Color>("You travel down the right path.\n\n" +
-                            "", Color.Lime));
-                    }
-                },
-
-
-
-                //forward
-                new ConsoleCommand("forward")
-                {
-                    OnCommand = args =>
-                    {
-                        if (State != 1) return;
-                        State = forward;
-                        Renderer.Console.MessageQueue.Enqueue(new KeyValuePair<string, Color>("Not really caring to make a decision you forage straight forward into the forest.\n\n" +
-                            "", Color.Lime));
-                    }
-                },
-
-
-
-                //back
-                new ConsoleCommand("back")
-                {
-                    OnCommand = args =>
-                    {
-                        if (State != 1) return;
-                        State = back;
-                        Renderer.Console.MessageQueue.Enqueue(new KeyValuePair<string, Color>("You begin traveling backwards but fucked up, you are traveling backwards in time.\n\n" +
-                            "", Color.Lime));
-                    }
-                },
-
-
-
-                //up
-                new ConsoleCommand("up")
-                {
-                    OnCommand = args =>
-                    {
-                        if (State != 1) return;
-                        State = up;
-                        Renderer.Console.MessageQueue.Enqueue(new KeyValuePair<string, Color>("Despite usually being bound to earth by gravity you manage to start floating straight up.\n\n" +
-                            "", Color.Lime));
-                    }
-                },
-
-
-
-                //down
-                new ConsoleCommand("down")
-                {
-                    OnCommand = args =>
-                    {
-                        if (State != 1) return;
-                        State = down;
-                        Renderer.Console.MessageQueue.Enqueue(new KeyValuePair<string, Color>("You begin burrowing into the ground. " +
-                            "A few miles down you turn around to find shrek is behind you and is helping you dig. " +
-                            "You continue until you hit the mantle. " +
-                            "Shrek advises you turn back, what shall you do? (back, cointinue)\n\n" +
-                            "", Color.Lime));
-                    }
-                },
-
-                new ConsoleCommand("back")
-                {
-                    OnCommand = args =>
-                    {
-                        if (State != down) return;
-                        State = down + 100;
-                        Renderer.Console.MessageQueue.Enqueue(new KeyValuePair<string, Color>("You turn back and go to the surface, " +
-                            "only to find that you are no longer on earth but on a giant onion in space with many layers. " +
-                            "Lord fuckwad sent space pirates to space assasinate you, how do you retaliate? (golden ticket, golden onion, railgun, microwave gun)\n\n" +
-                            "", Color.Lime));
-                    }
-                },
-                new ConsoleCommand("golden ticket")
-                {
-                    OnCommand = args =>
-                    {
-                        if (State != down) return;
-                        State = down + 100 + 10;
-                        Renderer.Console.MessageQueue.Enqueue(new KeyValuePair<string, Color>("You pull a golden ticket out of seemingly nowhere and flail it around. " +
-                            "The polar express appears from behind a asteroid and runs over all the space pirates. It then stops to pick you up, then heads toward the north pole.\n\n" +
-                            "" +
-                            "Once you reach the north pole you realize that all the elfs have been enslaved by Adolf Hitler, " +
-                            "and the nazis are going to board the train and search for more elfs. " +
-                            "You and shrek hop off just outside of town and meet up with the rebels trying to kill Hitler, who are being lead by Santa. " +
-                            "His weapons storehouse is unlike anything you've ever seen; tanks, planes, nukes. " +
-                            "You name it he had it. " +
-                            "His spys inform you all that now is a good time to strike, however Santa wants to perform some ritual before you go. " +
-                            "Whats the plan? (ritual, nuke, sneaky, siege)\n\n" +
-                            "", Color.Lime));
-                    }
-                },
-                new ConsoleCommand("golden onion")
-                {
-                    OnCommand = args =>
-                    {
-                        if (State != down) return;
-                        State = down + 100 + 10;
-                        Renderer.Console.MessageQueue.Enqueue(new KeyValuePair<string, Color>("You eat the golden onion. " +
-                            "You feel as though you can fart the end of the world. " +
-                            "Your fart is so powerful it could be compared to a sun once it has reached the end of its life. " +
-                            "Everything is destoyed in the wake of your nuclear fart, even you.\n\n" +
-                            "", Color.Lime));
-                    }
-                },
-
-                new ConsoleCommand("continue")
-                {
-                    OnCommand = args =>
-                    {
-                        if (State != down) return;
-                        State = down + 200;
-                        Renderer.Console.MessageQueue.Enqueue(new KeyValuePair<string, Color>("You tell shrek you must continue, he understands and follows you down. " +
-                            "You continue through the mantle until you reach an opening. You have found Hell. " +
-                            "Where do you head next? (lava, center)\n\n" +
-                            "", Color.Lime));
-                    }
-                },
-
-
-
-                //die
-                new ConsoleCommand("die")
-                {
-                    OnCommand = args =>
-                    {
-                        if (State != 1) return;
-                        State = die;
-                        Renderer.Console.MessageQueue.Enqueue(new KeyValuePair<string, Color>("You drop dead instantly. " +
-                            "You awake to the sweet, stinging scent of onions a few hours later and find shrek has revived you. (kill, dream)\n\n" +
-                            "", Color.Lime));
-                    }
-                },
-            });
-        }
-
-
-        #endregion
-
-
     }
 }
