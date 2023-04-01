14ce9fedaf82cf083f3dea3cac235e4735368dd1
Shawdooow <banhammer1915@gmail.com>
2023-04-01 13:17:05 -0400

    lots of assets

--- Vitaru.Mods.Shrekdeavor/ShrekStates.cs
+++ Vitaru.Mods.Shrekdeavor/ShrekStates.cs
@@ -27,6 +27,9 @@ namespace Vitaru.Mods.Shrekdeavor
             foreach (StartStates s in Enum.GetValues(typeof(StartStates)))
                 list.Add(GetState(s));
 
+            foreach (LeftStates s in Enum.GetValues(typeof(LeftStates)))
+                list.Add(GetState(s));
+
             foreach (DownStates s in Enum.GetValues(typeof(DownStates)))
                 list.Add(GetState(s));
 
@@ -87,8 +90,15 @@ namespace Vitaru.Mods.Shrekdeavor
 
 
 
-        [Description("You walk down the left path.\n\n" +
-                ""), 
+        [Description("You walk down the left path. " +
+                "After walking for some time you come upon a large white castle making quite a ruckus. " +
+                "Inside you find lord fuckwad is hosting a WWE tournament with many contestants. " +
+                "You recognize Shrek, an old friend and approach him. " +
+                "Shrek greets you and informs you he is next up and must choose an opponent. " +
+                "He asks for your advice.\n\n" +
+                "(Master Chief, Jack Black, Albert Wesker, Imposter, John Wick, " +
+                "Lady Dimitrescu, Walter White, Thanos, Megamind, Minions, Grinch, " +
+                "The Mask, Mr Incredible, Myers, Putin, Kim Jong Un, Xi Jinping, Elmo, Gru, Pinhead)"), 
             InputState((ulong)Shrekdeavor)]
         Left = 1000,
 
@@ -125,6 +135,110 @@ namespace Vitaru.Mods.Shrekdeavor
             "You awake to the sweet, stinging scent of onions a few hours later and find shrek has revived you. (kill, dream)\n\n"), 
             InputState((ulong)Shrekdeavor)]
         Die = 7000,
+    }
+
+    public enum LeftStates
+    {
+        [Description("Shrek enters the arena with WWE legend Master Chief, he looks tough. " +
+            "Shrek is a bit nervous about this one. " +
+            "What strategy should he use?\n\n" +
+            "(Aggressive, Defensive, Retarded)"),
+            InputState((ulong)StartStates.Left), InputText("Master Chief")]
+        MasterChief = StartStates.Left + 10,
+
+        [Description("Shrek enters the arena with the king of hell himself, he looks tough. " +
+            "Shrek is a bit nervous about this one. " +
+            "What strategy should he use?\n\n" +
+            "(Aggressive, Defensive, Retarded)"),
+            InputState((ulong)StartStates.Left), InputText("Jack Black")]
+        JackBlack = StartStates.Left + 20,
+
+        [Description("Shrek enters the arena with some freak in a leather jacket, he looks like a joke. " +
+            "Shrek is confident about this one. " +
+            "What strategy should he use?\n\n" +
+            "(Aggressive, Defensive, Retarded)"),
+            InputState((ulong)StartStates.Left), InputText("Albert Wesker")]
+        AlbertWesker = StartStates.Left + 30,
+
+        [Description("Shrek enters the arena with a short space man wielding a chef's knife, he looks like a joke. " +
+            "Shrek is confident about this one. " +
+            "What strategy should he use?\n\n" +
+            "(Aggressive, Defensive, Retarded)"),
+            InputState((ulong)StartStates.Left)]
+        Imposter = StartStates.Left + 40,
+
+        [Description(""),
+            InputState((ulong)StartStates.Left), InputText("John Wick")]
+        JohnWick = StartStates.Left + 50,
+
+        [Description(""),
+            InputState((ulong)StartStates.Left), InputText("Lady Dimitrescu")]
+        LadyDimitrescu = StartStates.Left + 60,
+
+        [Description(""),
+            InputState((ulong)StartStates.Left), InputText("Walter White")]
+        WalterWhite = StartStates.Left + 70,
+
+        [Description("Shrek enters the arena with taller and more in-shape purple variant of himself, he looks pissed. " +
+            "Shrek is confused about this one. " +
+            "What strategy should he use?\n\n" +
+            "(Aggressive, Defensive, Retarded)"),
+            InputState((ulong)StartStates.Left)]
+        Thanos = StartStates.Left + 80,
+
+        [Description("Shrek enters the arena with a blue dude who has terminal 7 brain cancer, he looks retarded. " +
+            "Shrek is conflicted about this one. " +
+            "What strategy should he use?\n\n" +
+            "(Aggressive, Defensive, Retarded)"),
+            InputState((ulong)StartStates.Left)]
+        Megamind = StartStates.Left + 90,
+
+        [Description(""),
+            InputState((ulong)StartStates.Left)]
+        Minions = StartStates.Left + 100,
+
+        [Description(""),
+            InputState((ulong)StartStates.Left)]
+        Grinch = StartStates.Left + 110,
+
+        [Description("Shrek enters the arena with Jim Carrey's The Mask, he looks menacing. " +
+            "Shrek is afraid of this one. " +
+            "What strategy should he use?\n\n" +
+            "(Aggressive, Defensive, Retarded)"),
+            InputState((ulong)StartStates.Left)]
+        Mask = StartStates.Left + 120,
+
+        [Description(""),
+            InputState((ulong)StartStates.Left), InputText("Mr. Incredible")]
+        MrIncredible = StartStates.Left + 130,
+
+        [Description(""),
+            InputState((ulong)StartStates.Left)]
+        Myers = StartStates.Left + 140,
+
+        [Description(""),
+            InputState((ulong)StartStates.Left)]
+        Putin = StartStates.Left + 150,
+
+        [Description(""),
+            InputState((ulong)StartStates.Left), InputText("Kim Jong Un")]
+        KimJongUn = StartStates.Left + 160,
+
+        [Description(""),
+            InputState((ulong)StartStates.Left), InputText("Xi Jinping")]
+        XiJinping = StartStates.Left + 170,
+
+        [Description(""),
+            InputState((ulong)StartStates.Left)]
+        Elmo = StartStates.Left + 180,
+
+        [Description(""),
+            InputState((ulong)StartStates.Left)]
+        Gru = StartStates.Left + 190,
+
+        [Description(""),
+            InputState((ulong)StartStates.Left)]
+        Pinhead = StartStates.Left + 200,
     }
 
     public enum DownStates
